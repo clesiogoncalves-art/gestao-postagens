14d647a9184157921a382e2235d9207cfbe16bda
clesiogoncalves-art <clesio.goncalves@sillion.com.br>
2026-09-17 15:22:25 -0300

    Update sinc.js

--- sinc.js
+++ sinc.js
@@ -1,11 +1,2 @@
-export async function onRequest(context) {
-    // Lê a variável de ambiente cadastrada no Cloudflare
-    const token = context.env.CLICKUP_TOKEN || context.env.CORREIOS_USER;
 
-    return new Response(JSON.stringify({ message: "Backend conectado com sucesso!", token_status: !!token }), {
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        }
-    });
 }
